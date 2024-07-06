23f765617765893a2df0b787a6c1cca4b58ae398
Vishesh Namdev <115422345+VisheshNamdev@users.noreply.github.com>
2024-07-06 10:50:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from website import create_app
 
+app = create_app()
 if __name__ == "__main__":
     # Create the Flask app instance
-    app = create_app()
     # Run the Flasṇk app with debug mode enabled
     app.run(debug=True)
